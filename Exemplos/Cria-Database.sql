89b141ea6ae60ad9a9d2d5863d1d551d10468bbd
Ana Mercedes Gauna <amgauna@gmail.com>
2017-04-15 03:32:18 -0300

    Create Cria-Database.sql

---
+++ Exemplos/Cria-Database.sql
@@ -0,0 +1,46 @@
+USE master;
+GO
+-- Create the database with the default data
+-- filegroup and a log file. Specify the
+-- growth increment and the max size for the
+-- primary data file.
+CREATE DATABASE MyDB
+ON PRIMARY
+  ( NAME='MyDB_Primary',
+    FILENAME=
+       'c:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\data\MyDB_Prm.mdf',
+    SIZE=4MB,
+    MAXSIZE=10MB,
+    FILEGROWTH=1MB),
+FILEGROUP MyDB_FG1
+  ( NAME = 'MyDB_FG1_Dat1',
+    FILENAME =
+       'c:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\data\MyDB_FG1_1.ndf',
+    SIZE = 1MB,
+    MAXSIZE=10MB,
+    FILEGROWTH=1MB),
+  ( NAME = 'MyDB_FG1_Dat2',
+    FILENAME =
+       'c:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\data\MyDB_FG1_2.ndf',
+    SIZE = 1MB,
+    MAXSIZE=10MB,
+    FILEGROWTH=1MB)
+LOG ON
+  ( NAME='MyDB_log',
+    FILENAME =
+    'c:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\data\MyDB.ldf',
+    SIZE=1MB,
+    MAXSIZE=10MB,
+    FILEGROWTH=1MB);
+GO
+ALTER DATABASE MyDB 
+  MODIFY FILEGROUP MyDB_FG1 DEFAULT;
+GO
+
+-- Create a table in the user-defined filegroup.
+USE MyDB;
+CREATE TABLE MyTable
+  ( cola int PRIMARY KEY,
+    colb char(8) )
+ON MyDB_FG1;
+GO
