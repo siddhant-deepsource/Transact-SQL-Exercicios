dacd743d76a6c72338f72568a43f2975cb83389d
Ana Mercedes Gauna <amgauna@gmail.com>
2017-06-16 00:30:41 -0300

    Create DML.sql

---
+++ Exemplos/DML.sql
@@ -0,0 +1,12 @@
+// Exemplo de DML (Linguagem de manipulação de dados)
+
+// Leia as materias que o aluno "João Lopes" está estudando.
+
+find any alunos using "João Lopes"
+find first materias with  estuda
+// faça enquanto houver matéria
+get materia 
+// ou get estuda
+// imprime materia 
+find next within estudar
+// fim enquanto
