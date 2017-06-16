60938e226308c8c32ffb156a32d982b3b82255ad
Ana Mercedes Gauna <amgauna@gmail.com>
2017-06-16 00:08:30 -0300

    Create estrela-azul.sql

---
+++ Exemplos/estrela-azul.sql
@@ -0,0 +1,15 @@
+//  1) Leia , imprima e grave os moradores do prédio "Estrela Azul"
+//  2) Leia e imprima todos os prédios da Rua Gusmão com seus moradores.
+//  3) O morador do apto 102 mudou para o apto. 1310 do prédio 100.
+//     Faça esta troca, salve e imprima.
+
+
+find any predio using "Estrela Azul"
+find first morador withing tem
+// enquanto houver morador faça
+get tem
+// imprimir tem
+modify tem
+find next morador withing tem
+//fim enquanto
+
