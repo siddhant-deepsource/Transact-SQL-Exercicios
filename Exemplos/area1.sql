662422f2376c059fb26b612f789e8616ea6ca0f8
Ana Mercedes Gauna <amgauna@gmail.com>
2017-06-16 00:43:32 -0300

    Create area1.sql

---
+++ Exemplos/area1.sql
@@ -0,0 +1,46 @@
+
+select 1
+use clientes
+selec 2
+use fornec
+ondex on (nome + dtoc(dt_nasc)) tag chave1
+set safety on
+set order to chave1
+set order to chave2
+select3
+use tabela1 in 4
+use
+select 20
+use
+close all // fecha todas as tabelas, janelas, arquivos, etc...
+
+select tab1
+use tabela3 in 3
+
+use tabela1 again alias tab1
+
+use tab1 // abe
+use // fecha
+
+select tabx
+use tabela1 alias tab1 in 1
+
+// abre tabela na área disponível
+
+select tab1
+use tabela1 again in 0
+
+close all
+
+use cliente in 0
+list all
+index on nome tag nomecli
+index on dt_nsc tag dtnsc
+set order to nome_cli
+set order to dtnsc
+use cliente agian alias cII in 0
+set order to dtnsc
+
+// criar arquivo de indice
+
+index on nome to ind_nome
